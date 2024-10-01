672896b1559c622ad1722f702a698718d8db9169
Константин Абрамов <KonstantiNAbramoV2017@yandex.ru>
2024-10-01 19:24:06 +0300

    optimize key logic

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,14 @@
 from random import randint
 number = randint(1, 100)
-WELKOM = 1
 
 def main():
     while True:
         guess = int(input("Введите число: "))
         if guess < number:
             print("Ваше число меньше того, что загадано.")
-        if guess > number:
+        elif guess > number:
             print("Ваше число больше того, что загадано.")
-        if guess == number:
+        elif guess == number:
             break
 
 main()
